09d37831de579d883cc7c310aabd3f769a7c13a6
ms-luc <36711108+ms-luc@users.noreply.github.com>
2018-06-02 13:00:25 -0400

    Update ReadWebPage.java

--- ReadWebPage.java
+++ ReadWebPage.java
@@ -9,13 +9,17 @@ public class ReadWebPage {
 
 	public static void search(String webURL, String word) throws IOException {
 		
+		// print the full url
 		System.out.println(webURL + word);
 		
+		// create full url object
 		URL url = new URL(webURL + word);
-        InputStream is =  url.openStream();
+        	InputStream is =  url.openStream();
 		
+		// create buffered stream
 		BufferedReader buffRead = new BufferedReader(new InputStreamReader(is));
 
+		// read buffered stream line by line and output to console
 		String line;
 		while ((line = buffRead.readLine()) != null) 
 			System.out.println(line);
@@ -23,12 +27,9 @@ public class ReadWebPage {
 	}
     public static void main(String[] args) throws IOException {
 		
-		//URL url = new URL("http://www.thesaurus.com/misspelling?term=hel");
-		//InputStream is =  url.openStream();
-		
-		if(args.length != 0)
-		
-			search("http://www.thesaurus.com/browse/", args[0]);
+	// if input is not null
+	if(args.length != 0)
+		search("http://www.thesaurus.com/browse/", args[0]);
         
     }
 
